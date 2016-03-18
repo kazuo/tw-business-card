43e9ec9b48e0dd0f91d20801ca2c1bebfce9cf7b
Rey Marin Jr <rey.marin@gmail.com>
2016-03-18 00:07:22 -0700

    Move jspm config to root

--- test/directives/twBusinessCard.spec.js
+++ test/directives/twBusinessCard.spec.js
@@ -1,6 +1,12 @@
 'use strict';
 
+import 'angular';
+//import 'angular-mock';
+
 describe('twBusinessCard directive', function () {
+
+    //beforeEach(module('twinwork.twBusinessCard'));
+
     it('should do something', function () {
 
     });
